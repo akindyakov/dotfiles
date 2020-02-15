1cecb0a93b7bbc6561b10612741eea21216c0537
akindyakov <akindyakov@gmail.com>
2020-02-15 21:52:03 +0000

    Fix image sorter: incorrect data detection and faulty error handling

--- bin/image-sorter.py
+++ bin/image-sorter.py
@@ -2,6 +2,7 @@ from piexif._exceptions import InvalidImageDataError
 import piexif
 
 import datetime
+import hashlib
 import logging
 import os
 import platform
@@ -32,7 +33,7 @@ def get_tags_by_names(names):
 
 _dates_tags = get_tags_by_names(_dates_names)
 
-def creation_date(path_to_file):
+def file_creation_date(path_to_file):
     """
     Try to get the date that a file was created, falling back to when it was
     last modified if that isn't possible.
@@ -52,7 +53,7 @@ def creation_date(path_to_file):
 dd_mm_yyyy_re = re.compile("([0123]\d)[:,.;-_]([01]\d)[:,.;-_]([12][09]\d\d)")
 yyyy_mm_d_re = re.compile("([12][09]\d\d)[:,.;-_]([01]\d)[:,.;-_]([0123]\d)")
 dd_mm_yy_re = re.compile("([0123]\d)[:,.;-_]([01]\d)[:,.;-_](\d\d)")
-yyyy_re = re.compile("(2\d|19)(\d\d)")
+yyyy_re = re.compile("(20|19)(\d\d)")
 
 def guess_date_from_str(s):
     r = dd_mm_yyyy_re.search(s)
@@ -89,7 +90,7 @@ def guess_date_from_str(s):
     if r is not None:
         y = r.group()
         try:
-            dt = datetime.datetime(year=int(y) + 2000, month=1, day=1)
+            dt = datetime.datetime(year=int(y), month=1, day=1)
             return dt
         except ValueError:
             pass
@@ -116,17 +117,23 @@ def guess_create_time(file_path):
                     logging.warning("Unexpected date format {!r} in file {!r}".format(value.decode("utf-8"), file_path))
     except InvalidImageDataError as e:
         logging.warning("Not an image {!r}".format(e))
+    except Exception as e:
+        logging.warning("Exif extraction error {!r}".format(e))
     if not dates:
-        d = guess_date_from_str(os.path.basename(file_path))
+        basename = os.path.basename(file_path)
+        d = guess_date_from_str(basename)
         if d is not None:
+            logging.warning("Took date {!r} from filename {!r}".format(d, basename))
             dates.append(d)
         else:
             d = guess_date_from_str(file_path)
             if d is not None:
+                logging.warning("Took date {!r} from filename {!r}".format(d, file_path))
                 dates.append(d)
     if not dates:
-        logging.warning("Creation date is absent in exif attributes, use filesystem modification time")
-        dates.append(creation_date(file_path))
+        d = file_creation_date(file_path)
+        logging.warning("Took date {!r} from filesystem for file {!r}".format(d, file_path))
+        dates.append(d)
     return min(dates)
 
 
@@ -140,7 +147,7 @@ def walk(src_dir, dst_dir):
             process_file(file_path, dst_dir)
 
 
-def pick_up_filename(dir_path, basename):
+def pick_up_filename(dir_path, basename, keep_unique):
     if basename.startswith("."):
         basename = basename[1:]
     if basename.startswith("~"):
@@ -148,7 +155,7 @@ def pick_up_filename(dir_path, basename):
     basename = basename.replace("  ", " ").replace("  ", " ").replace("  ", " ")
     basename = basename.replace(" ", "-")
     new_file_name = os.path.join(dir_path, basename)
-    if os.path.exists(new_file_name):
+    if keep_unique and os.path.exists(new_file_name):
         num = 0
         bare_basename, ext = os.path.splitext(basename)
         while os.path.exists(new_file_name):
@@ -159,18 +166,37 @@ def pick_up_filename(dir_path, basename):
             )
     return new_file_name
 
+_md5_cache = {}
+
+def md5(fname, use_cache=False):
+    hash_md5 = hashlib.md5()
+    if fname not in _md5_cache:
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        if not use_cache:
+            return hash_md5.hexdigest()
+        _md5_cache[fname] = hash_md5.hexdigest()
+    return _md5_cache[fname]
 
 def process_file(file_path, dst_dir):
     bdate = guess_create_time(file_path)
     dst_dir = os.path.join(dst_dir, bdate.strftime("%Y/%b/%d-%a"))
     os.makedirs(dst_dir, exist_ok=True)
     basename = os.path.basename(file_path)
-    new_file_name = pick_up_filename(dst_dir, basename)
-    logging.warning("copy: {} {}".format(file_path, new_file_name))
+    new_file_name = pick_up_filename(dst_dir, basename, False)
+    if os.path.exists(new_file_name):
+        if md5(new_file_name, True) == md5(file_path):
+            logging.warning("skip repeating file: {} -> {}".format(file_path, new_file_name))
+            return
+        else:
+            new_file_name = pick_up_filename(dst_dir, basename, True)
+    logging.info("copy: {} {}".format(file_path, new_file_name))
     shutil.copyfile(file_path, new_file_name)
 
 src_dir = sys.argv[1]
 dst_dir = sys.argv[2]
+
 logging.warning("Src {}".format(src_dir))
 logging.warning("Dst {}".format(dst_dir))
 
